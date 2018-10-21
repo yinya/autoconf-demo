e90c0fbc3d35c1aaf1d35b833ac8178684895281
Parallels <parallels@centos-7-04-4-link.shared>
2018-10-21 11:24:47 +0800

    first commit

---
+++ demo-01/include/pr.h
@@ -0,0 +1,14 @@
+//
+// Created by parallels on 10/21/18.
+//
+
+#ifndef DEMO_01_PR_H
+#define DEMO_01_PR_H
+
+int pr()
+{
+    printf("helloworld");
+    return 0;
+}
+
+#endif //DEMO_01_PR_H
---
+++ demo-01/src/hello.c
@@ -0,0 +1,11 @@
+//
+// Created by parallels on 10/21/18.
+//
+#include "pr.h"
+int main()
+{
+    int x;
+    x=pr();
+    return 0;
+}
+
